32b18627bc1e07d4921fcb9a0fede4c2f619085a
xmirk <irwin13@gmail.com>
2015-06-25 16:37:50 +0700

    finish CRUD for student

--- sis/controller/StudentController.php
+++ sis/controller/StudentController.php
@@ -44,3 +44,44 @@ $app->post('/student-create', function() use ($app) {
 		echo '{"error":{"text":'. $e->getMessage() .'}}';
 	}
 });
+
+$app->get('/student-edit/:id', function($id) use ($app) {
+    $sql = "SELECT id, first_name, last_name, nim FROM student WHERE id = ?";
+	try {
+		$db = getDbConnection();
+		$stmt = $db->prepare($sql); 
+		$stmt->execute([$id]);
+		$result = $stmt->fetch(PDO::FETCH_ASSOC);
+		$db = null;
+				
+		$app->render(
+			'view/studentEdit.tpl.html',
+			array(
+				'model' => $result
+			)
+		);
+	} catch (PDOException $e) {
+		//error_log($e->getMessage(), 3, '/var/tmp/phperror.log'); //Write error log
+		echo '{"error":{"text":'. $e->getMessage() .'}}';
+	}
+});
+
+$app->post('/student-edit', function() use ($app) {
+    $sql = "UPDATE student SET first_name = ?, last_name = ?, nim = ? WHERE id = ?";
+	try {
+		$db = getDbConnection();
+		$first_name = $app->request()->post('first_name');
+		$last_name = $app->request()->post('last_name');
+		$nim = $app->request()->post('nim');
+		$id = $app->request()->post('id');
+		
+		$stmt = $db->prepare($sql); 
+		$stmt->execute([$first_name, $last_name, $nim, $id]);
+		$db = null;
+				
+		$app->redirect('students');
+	} catch (PDOException $e) {
+		//error_log($e->getMessage(), 3, '/var/tmp/phperror.log'); //Write error log
+		echo '{"error":{"text":'. $e->getMessage() .'}}';
+	}
+});
