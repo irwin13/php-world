cab78714c979ce6e09ad98de85d21629d00f9c13
irwin13 <irwin13@gmail.com>
2015-11-25 16:32:23 +0700

    finish xml dynamic call sp

--- sws/controller/WsController.php
+++ sws/controller/WsController.php
@@ -3,35 +3,57 @@
 require_once 'db/DbConnection.php';
 
 /*
-<message>
-  <functionId></functionId>
+<inputMessage>
+  <functionId>PROC_WITH_CURSOR</functionId>
   <parameterList>
     <parameter>
-      <name>param1</name>
-      <value>irwin</value>
+      <name>number</name>
+      <value>13</value>
     </parameter>
     <parameter>
-      <name>param2</name>
-      <value>bekasi</value>
+      <name>char</name>
+      <value>irwin</value>
     </parameter>    
   </parameterList>
-</message>
+</inputMessage>
 */
 $app->post('/ws', function() use ($app) {
-    $sql = 'begin PROC_WITH_CURSOR(:number, :cursbv); end;';
+    
+	// call oracle store procedure syntax = 'begin PROC_WITH_CURSOR(:param1, :param2, :out_cursor); end;';
+	
+	$sql = 'begin ';
 	
 	// read http body
 	$http_body = @file_get_contents('php://input');
 	
-	// xml parsing
-	$xml = new SimpleXMLElement($http_body);
-	echo "functionId = {$xml->functionId} \n";
+	// xml parsing	
+	$parser = xml_parser_create();
+    xml_parser_set_option($parser, XML_OPTION_CASE_FOLDING, 0);
+    xml_parser_set_option($parser, XML_OPTION_SKIP_WHITE, 1);	
+	xml_parse_into_struct($parser, $http_body, $xmlValue, $xmlTag);
+	xml_parser_free($parser);
 	
-	foreach ($xml->parameterList->parameter as $element) {
-	  foreach($element as $key => $val) {
-	   echo "parameter name={$key} : value={$val} \n";
-	  }
-	}	
+	//echo "\nTags array\n";
+	//print_r($xmlTag);	
+	//echo "\nVals array\n";
+	//print_r($xmlValue);
+	
+	$functionId = $xmlValue[1]['value'];
+	echo "functionId = {$functionId} \n";
+	$sql .= $functionId . "(";
+
+	foreach($xmlTag as $key => $value) {
+		if ($key == 'name') {
+			$arrayIndex = $value;
+			for ($i = 0; $i < count($arrayIndex); $i++) {
+				$index = $arrayIndex[$i];
+				$sql .= ':'.$xmlValue[$index]['value'].', ';
+			}
+		}
+	}
+	
+	$sql .= ':out_cursor); end;';
+	echo "call Store Procedure = {$sql} \n";
 	
 	try {
 		$results = array();
@@ -39,27 +61,45 @@ $app->post('/ws', function() use ($app) {
 		$conn = getDbConnection();
 		$curs = oci_new_cursor($conn);
 		$stid = oci_parse($conn, $sql);
+		
 		//oci_bind_by_name($stid, ":number", $num); // untuk input maxlength dan type bisa dikosongkan, default adalah -1 dan SQLT_CHR
-		oci_bind_by_name($stid, ":number", $num, -1, OCI_B_INT); // eksplisit menyebut type adalah INT
-		//oci_bind_by_name($stid, ":number", $num, -1, SQLT_CHR); // default type adalah VARCHAR /CHAR ,masih bisa, php akan melakukan konversi otomatis
-		oci_bind_by_name($stid, ":cursbv", $curs, -1, OCI_B_CURSOR); // untuk out harus di define type OCI_B_CURSOR
+		//oci_bind_by_name($stid, ":number", $num, -1, OCI_B_INT); // eksplisit menyebut type adalah INT
+		//oci_bind_by_name($stid, ":number", $num, -1, SQLT_CHR); // default type adalah VARCHAR / CHAR ,masih bisa, php akan melakukan konversi otomatis
+						
+		foreach($xmlTag as $key => $value) {
+			if ($key == 'name') {
+				$arrayIndex = $value;
+				for ($i = 0; $i < count($arrayIndex); $i++) {
+					$index = $arrayIndex[$i];
+					echo "oci binding parameter :{$xmlValue[$index]['value']} with value = {$xmlValue[$index + 1]['value']} \n";				
+					oci_bind_by_name($stid, ":".$xmlValue[$index]['value'], $xmlValue[$index + 1]['value']);
+				}
+			}
+		}
+				
+		oci_bind_by_name($stid, ":out_cursor", $curs, -1, OCI_B_CURSOR); // untuk out harus di define type OCI_B_CURSOR
+		
 		oci_execute($stid);
 		oci_execute($curs);
 						
 		while (($row = oci_fetch_array($curs, OCI_ASSOC+OCI_RETURN_NULLS)) != false) {
 			$results[] = $row;
 		}				
+		
 		echo json_encode($results);
-		$xmlOutput = new SimpleXMLElement('<rootTag/>');
-		array_to_xml($xmlOutput, $results[0]);
+		
+		$xmlOutput = new SimpleXMLElement('<outputMessage/>');
+		array_to_xml($xmlOutput, $results);
+		
 		print $xmlOutput->asXML();
 		
-		oci_free_statement($stid);
-		oci_free_statement($curs);
-		oci_close($conn);
 	} catch (Exception $e) {
 		//error_log($e->getMessage(), 3, '/var/tmp/phperror.log'); //Write error log
 		echo '{"error":{"text":'. $e->getMessage() .'}}';
+	} finally {
+		oci_free_statement($stid);
+		oci_free_statement($curs);
+		oci_close($conn);		
 	}
 });
 
