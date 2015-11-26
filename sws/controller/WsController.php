51a092c7dbbf32974317ac8668edaa20b8ed4401
irwin13 <irwin13@gmail.com>
2015-11-26 14:28:23 +0700

    add query to mapping table

--- sws/controller/WsController.php
+++ sws/controller/WsController.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once 'db/DbConnection.php';
-
+// SQL EXAMPLE for call oracle store procedure  = 'begin PROC_WITH_CURSOR(:param1, :param2, :out_cursor); end;';
 /*
 input format 
 <inputMessage>
@@ -41,131 +41,156 @@ output format
 
 $app->post('/ws', function() use ($app) {
     
-	// call oracle store procedure syntax = 'begin PROC_WITH_CURSOR(:param1, :param2, :out_cursor); end;';
 	
-	$sql = 'begin ';
-	
-	// read http body
-	$http_body = @file_get_contents('php://input');
-	$xmlOutput = new SimpleXMLElement('<outputMessage/>');
-	
-	// xml parsing	
-	$parser = xml_parser_create();
-    xml_parser_set_option($parser, XML_OPTION_CASE_FOLDING, 0);
-    xml_parser_set_option($parser, XML_OPTION_SKIP_WHITE, 1);	
-	xml_parse_into_struct($parser, $http_body, $xmlValue, $xmlTag);
-	xml_parser_free($parser);
-	
-	//echo "\nTags array\n";
-	//print_r($xmlTag);	
-	//echo "\nVals array\n";
-	//print_r($xmlValue);
-	
-	// TODO call validateUser function based on username and password in xml, if == 'Y' continue process, if == 'N' cancel process, return xml denied
-	$username = $xmlValue[1]['value'];
-	$password = $xmlValue[2]['value'];
-	echo "username = {$username} \n";
-	echo "password = {$password} \n";
-	
-	$validationStatus = validateUser($username, $password);
-	
-	if ($validationStatus === 'Y') {
+			
+	try {
+		
+		$sql = 'begin ';
+		
+		// read http body
+		$http_body = @file_get_contents('php://input');
+		$xmlOutput = new SimpleXMLElement('<outputMessage/>');
+		
+		// xml parsing	
+		$parser = xml_parser_create();
+		xml_parser_set_option($parser, XML_OPTION_CASE_FOLDING, 0);
+		xml_parser_set_option($parser, XML_OPTION_SKIP_WHITE, 1);	
+		xml_parse_into_struct($parser, $http_body, $xmlValue, $xmlTag);
+		xml_parser_free($parser);
+		
+		//echo "\nTags array\n";
+		//print_r($xmlTag);	
+		//echo "\nVals array\n";
+		//print_r($xmlValue);
+		
+		// call validateUser function based on username and password in xml, if == 'Y' continue process, if == 'N' cancel process, return xml denied
+		$username = $xmlValue[1]['value'];
+		$password = $xmlValue[2]['value'];
+		//echo "username = {$username} \n";
+		//echo "password = {$password} \n";
+		
+		$validationStatus = validateUser($username, $password);
+		
+		if ($validationStatus == 'Y') {
 
-		$functionId = $xmlValue[3]['value'];
-		//echo "functionId = {$functionId} \n";
-		$sql .= $functionId . "(";
-
-		foreach($xmlTag as $key => $value) {
-			if ($key == 'name') {
-				$arrayIndex = $value;
-				for ($i = 0; $i < count($arrayIndex); $i++) {
-					$index = $arrayIndex[$i];
-					$sql .= ':'.$xmlValue[$index]['value'].', ';
+			$functionId = $xmlValue[3]['value'];
+			$mappingTable = functionMapping($functionId);
+			//echo "functionId = {$functionId} \n";
+			//print_r($mappingTable);
+						
+			if (count($mappingTable) > 0) {
+				$functionMap = $mappingTable[0];
+				$sourceName = $functionMap['SOURCE_NAME'];				
+				$outputType = $functionMap['OUTPUT_TYPE'];
+				//echo "sourceName = {$sourceName} \n";
+				//echo "outputType = {$outputType} \n";
+				
+				$isCursor;
+				if ($outputType == 'cur') {
+					$isCursor = true;
+				} else {
+					$isCursor = false;
 				}
-			}
-		}
-		
-		$sql .= ':out_cursor); end;';
-		//echo "call Store Procedure = {$sql} \n";
-		
-		// query table mapping procedure : select * from sci_int_list_tbl;
-		// TODO query ke mapping table sebelum call store procedure
-		$isCursor = false;
-		$results = array();
-		
-		try {
-			$conn = getDbConnection();
-			$stid = oci_parse($conn, $sql);
-					
-			if ($isCursor) {
-				$curs = oci_new_cursor($conn);
-							
-				//oci_bind_by_name($stid, ":number", $num); // untuk input maxlength dan type bisa dikosongkan, default adalah -1 dan SQLT_CHR
-				//oci_bind_by_name($stid, ":number", $num, -1, OCI_B_INT); // eksplisit menyebut type adalah INT
-				//oci_bind_by_name($stid, ":number", $num, -1, SQLT_CHR); // default type adalah VARCHAR / CHAR ,masih bisa, php akan melakukan konversi otomatis
-								
+				
+				$sql .= $sourceName . "(";
+
 				foreach($xmlTag as $key => $value) {
 					if ($key == 'name') {
 						$arrayIndex = $value;
 						for ($i = 0; $i < count($arrayIndex); $i++) {
 							$index = $arrayIndex[$i];
-							//echo "oci binding parameter :{$xmlValue[$index]['value']} with value = {$xmlValue[$index + 1]['value']} \n";				
-							oci_bind_by_name($stid, ":".$xmlValue[$index]['value'], $xmlValue[$index + 1]['value'], 10000);
+							$sql .= ':'.$xmlValue[$index]['value'].', ';
 						}
 					}
 				}
+				
+				$sql .= ':out_cursor); end;';
+				//echo "call Store Procedure = {$sql} \n";
+				
+				$results = array();
+				$conn = getDbConnection();
+				$stid = oci_parse($conn, $sql);
 						
-				oci_bind_by_name($stid, ":out_cursor", $curs, 32000, OCI_B_CURSOR); // untuk out harus di define type OCI_B_CURSOR
-				
-				oci_execute($stid);
-				oci_execute($curs);
-				
-				while (($row = oci_fetch_array($curs, OCI_ASSOC+OCI_RETURN_NULLS)) != false) {
-					$results[] = $row;
-				}				
-				
-				echo json_encode($results);
-				
-				$xmlOutput->addChild('status', 'success');
-				$xmlOutput->addChild('message', 'success');
-				array_to_xml($xmlOutput, $results);			
+				if ($isCursor) {
+					// execute call to database with return type cursor
+					$curs = oci_new_cursor($conn);
+								
+					//oci_bind_by_name($stid, ":number", $num); // untuk input maxlength dan type bisa dikosongkan, default adalah -1 dan SQLT_CHR
+					//oci_bind_by_name($stid, ":number", $num, -1, OCI_B_INT); // eksplisit menyebut type adalah INT
+					//oci_bind_by_name($stid, ":number", $num, -1, SQLT_CHR); // default type adalah VARCHAR / CHAR ,masih bisa, php akan melakukan konversi otomatis
+									
+					foreach($xmlTag as $key => $value) {
+						if ($key == 'name') {
+							$arrayIndex = $value;
+							for ($i = 0; $i < count($arrayIndex); $i++) {
+								$index = $arrayIndex[$i];
+								//echo "oci binding parameter :{$xmlValue[$index]['value']} with value = {$xmlValue[$index + 1]['value']} \n";				
+								oci_bind_by_name($stid, ":".$xmlValue[$index]['value'], $xmlValue[$index + 1]['value'], 10000);
+							}
+						}
+					}
+							
+					oci_bind_by_name($stid, ":out_cursor", $curs, 32000, OCI_B_CURSOR); // untuk out harus di define type OCI_B_CURSOR
+					
+					oci_execute($stid);
+					oci_execute($curs);
+					
+					while (($row = oci_fetch_array($curs, OCI_ASSOC+OCI_RETURN_NULLS)) != false) {
+						$results[] = $row;
+					}				
+					
+					//echo json_encode($results);
+					
+					$xmlOutput->addChild('status', 'success');
+					$xmlOutput->addChild('message', 'success');
+					array_to_xml($xmlOutput->addChild('data'), $results);			
+					
+					oci_free_statement($stid);
+					oci_free_statement($curs);	
+					oci_close($conn);		
+				} else {
+					// execute call to database with return type varchar or number 
+					foreach($xmlTag as $key => $value) {
+						if ($key == 'name') {
+							$arrayIndex = $value;
+							for ($i = 0; $i < count($arrayIndex); $i++) {
+								$index = $arrayIndex[$i];
+								//echo "oci binding parameter :{$xmlValue[$index]['value']} with value = {$xmlValue[$index + 1]['value']} \n";				
+								oci_bind_by_name($stid, ":".$xmlValue[$index]['value'], $xmlValue[$index + 1]['value'], 10000);
+							}
+						}
+					}
+							
+					oci_bind_by_name($stid, ":out_cursor", $varOutput, 10000);
+					oci_execute($stid);
+
+					$xmlOutput->addChild('status', 'success');
+					$xmlOutput->addChild('message', 'success');
+					$xmlOutput->addChild('data', $varOutput);
+					//print "output response = ". $varOutput . "\n";			
+					
+					oci_free_statement($stid);
+					oci_close($conn);		
+				}
 				
 			} else {
-				foreach($xmlTag as $key => $value) {
-					if ($key == 'name') {
-						$arrayIndex = $value;
-						for ($i = 0; $i < count($arrayIndex); $i++) {
-							$index = $arrayIndex[$i];
-							//echo "oci binding parameter :{$xmlValue[$index]['value']} with value = {$xmlValue[$index + 1]['value']} \n";				
-							oci_bind_by_name($stid, ":".$xmlValue[$index]['value'], $xmlValue[$index + 1]['value'], 10000);
-						}
-					}
-				}
-						
-				oci_bind_by_name($stid, ":out_cursor", $varOutput, 10000);
-				oci_execute($stid);
-				$xmlOutput->addChild('status', 'success');
-				$xmlOutput->addChild('message', $varOutput);			
-				//print "output response = ". $varOutput . "\n";			
+				// functionId not found on database
+				$xmlOutput->addChild('status', 'error');
+				$xmlOutput->addChild('message', 'functionId '.$functionId.' not found');									
 			}
 
-		} catch (Exception $e) {
-			//error_log($e->getMessage(), 3, '/var/tmp/phperror.log'); //Write error log
-			//echo '{"error":{"text":'. $e->getMessage() .'}}';
+		} else {
+			// user validation failed, return 'N' from database
 			$xmlOutput->addChild('status', 'error');
-			$xmlOutput->addChild('message', $e->getMessage());
-		} finally {
-			oci_free_statement($stid);
-			if ($isCursor) {
-				oci_free_statement($curs);	
-			}		
-			oci_close($conn);		
+			$xmlOutput->addChild('message', 'invalid username or password');					
 		}
-	
-	} else {
+		
+	} catch (Exception $e) {
+		//error_log($e->getMessage(), 3, '/var/tmp/phperror.log'); //Write error log
+		//echo '{"error":{"text":'. $e->getMessage() .'}}';
 		$xmlOutput->addChild('status', 'error');
-		$xmlOutput->addChild('message', 'invalid username or password');					
-	}
+		$xmlOutput->addChild('message', $e->getMessage());
+	} 
 	
 	print $xmlOutput->asXML();
 
@@ -212,9 +237,36 @@ function validateUser($username, $password)
 		oci_free_statement($stid);
 		oci_close($conn);		
 	}
+	
 	return $response;
 }
 
+function functionMapping($id) {
+	
+    $sql = 'SELECT SOURCE_NAME, OUTPUT_TYPE FROM SCI_INT_LIST_TBL WHERE ID = :id';
+	try {
+		$results = array();
+		$conn = getDbConnection();
+		$stid = oci_parse($conn, $sql);
+		oci_bind_by_name($stid, ":id", $id);			
+		oci_execute($stid);
+						
+		while (false!==($row=oci_fetch_array($stid, OCI_BOTH)) ) {
+			$results[] = $row;
+		}				
+				
+	} catch (Exception $e) {
+		//echo '{"error functionMapping ":{"text":'. $e->getMessage() .'}}';
+	} finally {
+		oci_free_statement($stid);
+		oci_close($conn);		
+	}
+	
+	return $results;
+}
+
+
+// web-service to test validation function on database
 $app->post('/ws-function', function() use ($app) {
 	
 	print "validateUser = ".validateUser('sucofindo', '12345') . "\n";
--- sws/db/DbConnection.php
+++ sws/db/DbConnection.php
@@ -1,7 +1,7 @@
 <?php
 function getDbConnection() {
-	//$conn = oci_connect('belajar', 'belajar', 'localhost/orcl');
-	$conn = oci_connect('APPS', 'APPS', 'oradev.sucofindo.co.id:1527/dev');
+	$conn = oci_connect('belajar', 'belajar', 'localhost/orcl');
+	//$conn = oci_connect('APPS', 'APPS', 'oradev.sucofindo.co.id:1527/dev');
 	if (!$conn) {
 		$e = oci_error();
 		trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
